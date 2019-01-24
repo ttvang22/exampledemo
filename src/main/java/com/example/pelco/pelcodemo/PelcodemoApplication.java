0f153b9d11154f2343a43ef3871b11178e30be3a
Tou Tong Vang <ttvang22@gmail.com>
2019-01-24 11:57:31 -0800

    Initial Basic Commit

---
+++ src/main/java/com/example/pelco/pelcodemo/PelcodemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.pelco.pelcodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PelcodemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PelcodemoApplication.class, args);
+	}
+
+}
+
